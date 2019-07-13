32c600c1b339a112268cfeb224262a35651ced26
sergenp <sergen12343@gmail.com>
2019-07-13 20:18:33 +0300

    add server and client communication

--- server/server.py
+++ server/server.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Yes"
+@app.route("/register", methods=['POST'])
+def register():
+    return jsonify({
+        'message' : "Succesfully recieved",
+        'content' : request.get_json()
+    })
 
 if __name__ == "__main__":
 	app.run(port=8081)
