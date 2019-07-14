91e75991e8301697f15386c900d71b9426fc42ed
sergenp <sergen12343@gmail.com>
2019-07-14 09:19:35 +0300

    add flask-cors

--- server/server.py
+++ server/server.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+@cross_origin()
 @app.route("/register", methods=['POST'])
 def register():
     return jsonify({
