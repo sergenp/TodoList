8a9ad6f10681f5632558aa09a1e36de9a61d8d0a
sergenp <sergen12343@gmail.com>
2019-07-14 11:25:45 +0300

    changed user.id to user.user_id, add repr of Todos

--- server/models.py
+++ server/models.py
@@ -10,11 +10,11 @@ class User(db.Model):
 
 class Todos(db.Model):
     todo_id =  db.Column(db.Integer, primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
     todo_title = db.Column(db.String(255), nullable=False)
     todo_body = db.Column(db.String(255), nullable=False)
     completed = db.Column(db.Boolean, default=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}, {self.password}>'
+        return f'<Todo {self.todo_id} from User {user_id} Title: {todo_title} Body:\n{todo_body}\n Completed {self.completed}>'
 
