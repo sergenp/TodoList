12f936ee30883e80fe4f868a984b657b9c738ef8
sergenp <sergen12343@gmail.com>
2019-07-16 09:25:15 +0300

    get rid of subscribe method

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -39,10 +39,4 @@ const store = new Vuex.Store({
   plugins: [vuexPersist.plugin]
 })
 
-// Subscribe to store updates
-store.subscribe((mutation, state) => {
-  // Store the state object as a JSON string
-  localStorage.setItem('store', JSON.stringify(state))
-})
-
 export default store
